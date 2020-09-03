734464bed2602826a3bd3f900d88f86f14292816
Jon Betts <jon.betts@hypothes.is>
2020-09-03 15:54:07 +0100

    Add unit tests

---
+++ tests/unit/viahtml/wsgi_test.py
@@ -0,0 +1,9 @@
+from pywb.apps.frontendapp import FrontEndApp
+
+
+def test_it_exports_application():
+    # For fixtures to kick in we need to import this here, as all the work
+    # happens instantly
+    from viahtml.wsgi import application  # pylint: disable=import-outside-toplevel
+
+    assert isinstance(application, FrontEndApp)  # pragma: no cover
