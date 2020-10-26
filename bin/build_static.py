dcad47bcfa84ea9c3eddcf2e09c4df5a223b8900
Jon Betts <jon.betts@hypothes.is>
2020-10-26 12:55:29 +0000

    Nest in the static content to make room for root content
     * Previously /static in the directory was /static in the app
     * Now it resembles the root
     * Also treat `wombat.js` as a special case, rather than the default

--- bin/build_static.py
+++ bin/build_static.py
@@ -30,7 +30,7 @@ def _create_static(source, target):
 if __name__ == "__main__":
     # pylint: disable=invalid-name
     bin_dir = os.path.abspath(os.path.dirname(__file__))
-    root_dir = os.path.abspath(os.path.join(bin_dir, "../static/pywb"))
+    root_dir = os.path.abspath(os.path.join(bin_dir, "../static/static/pywb"))
 
     _create_static(
         source=resource_filename("pywb", "static"),
