8701589511928a34d9e2fb670965f2c3e703ed27
Marcos Prieto <marcospri@gmail.com>
2021-03-09 10:10:49 +0100

    Initialize sentry sdk on app startup

--- viahtml/wsgi.py
+++ viahtml/wsgi.py
@@ -1,7 +1,18 @@
 """The application providing a WSGI entry-point."""
+import os
 
 # Our job here is to leave this `application` attribute laying around as
 # it's what uWSGI expects to find.
 from viahtml.app import Application
 
-application = Application()  # pylint: disable=invalid-name
+application = Application()
+
+if os.environ.get("SENTRY_DSN"):  # pragma: no cover
+    # As both pywb and sentry shamelessly monkey patch gevent etc the order
+    # of imports matter. Importing sentry here results in the right patching.
+    import sentry_sdk
+    from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
+
+    # pylint: disable=redefined-variable-type
+    sentry_sdk.init(dsn=os.environ["SENTRY_DSN"])
+    application = SentryWsgiMiddleware(application)
