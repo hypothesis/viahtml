555da23c4e9e76c8458bbab3e76d6c63d6077f96
Robert Knight <robertknight@gmail.com>
2023-02-20 14:24:26 +0000

    Copy all static resources from pywb/static/ to static/pywb/ in Docker build
    Copy all resources instead of a specific list of files.
    
    This makes pywb version upgrades easier by not requiring this script to be
    updated as often when updating pywb versions.

--- bin/build_static.py
+++ bin/build_static.py
@@ -1,38 +1,9 @@
 """Copy all required static content out of `pywb` so NGINX can serve it."""
 import os
 import os.path
-from shutil import copyfile, rmtree
+from shutil import copytree, rmtree
 
 import importlib_resources
-
-# All the pywb resources we know we need
-MANIFEST = (
-    "autoFetchWorker.js",
-    "default_banner.js",
-    "flowplayer/toolbox.flashembed.js",
-    "js/bootstrap.min.js",
-    "js/jquery-latest.min.js",
-    "js/url-polyfill.min.js",
-    "query.js",
-    "queryWorker.js",
-    "search.js",
-    "transclusions.js",
-    "vidrw.js",
-    "wb_frame.js",
-    "wombat.js",
-    "wombatProxyMode.js",
-    "wombatWorkers.js",
-)
-
-
-def _copy(source_file, target_file):
-    print(f"{source_file} >>> {target_file}")
-
-    dir_name = os.path.dirname(target_file)
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-
-    copyfile(source_file, target_file)
 
 
 def _create_static(source, target):
@@ -40,14 +11,8 @@ def _create_static(source, target):
         print(f"Removing old static directory: {target}")
         rmtree(target)
 
-    print(f"Copying `pywb` static content: {source}")
-    for path in MANIFEST:
-        source_file, target_file = os.path.join(source, path), os.path.join(
-            target, path
-        )
-        _copy(source_file, target_file)
-
-    print(f"Created: {target}")
+    print(f"Copying `pywb` static content: {source} to {target}")
+    copytree(source, target)
 
 
 if __name__ == "__main__":
